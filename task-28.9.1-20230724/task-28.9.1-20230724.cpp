433de117261bff61b81201f320a817e3212cb2f9
7taras <7taras@mail.ru>
2023-07-25 18:59:28 +0300

    done

--- task-28.9.1-20230724/task-28.9.1-20230724.cpp
+++ task-28.9.1-20230724/task-28.9.1-20230724.cpp
@@ -12,6 +12,8 @@
 
 using namespace std;
 
+unsigned int numberOfConcurrentThreads;
+unsigned int test{ 0 };
 
 // Merges two subarrays of array[].
 // First subarray is arr[begin..mid]
@@ -74,13 +76,33 @@ void mergeSort(int array[], int const begin, int const end)
 {
     if (begin >= end)
         return;
+    
+    int mid = begin + (end - begin) / 2;
+    
+    if (numberOfConcurrentThreads)
+    {
+        numberOfConcurrentThreads--;
+        future<void> fut = async(launch::async, [&]() { mergeSort(array, begin, mid); });
+        mergeSort(array, mid + 1, end);
+        fut.get();
+    }
+    else
+    {
+        mergeSort(array, begin, mid);
+        mergeSort(array, mid + 1, end);
+    }
+    merge(array, begin, mid, end);
+    return;
+}
+
+void mergeSortWithoutThreads(int array[], int const begin, int const end)
+{
+    if (begin >= end)
+        return;
 
     int mid = begin + (end - begin) / 2;
-    //mergeSort(array, begin, mid);
-    future<void> fut1 = async(launch::async, [&]() { mergeSort(array, begin, mid); });
+    mergeSort(array, begin, mid);
     mergeSort(array, mid + 1, end);
-    fut1.get();
-    //future<void> fut2 = async([&]() { mergeSort(array, mid + 1, end); });
     merge(array, begin, mid, end);
     return;
 }
@@ -96,31 +118,40 @@ void printArray(int A[], int size)
 
 int main()
 {
+    numberOfConcurrentThreads = std::thread::hardware_concurrency();
+    std::cout << numberOfConcurrentThreads << " concurrent threads are supported.\n";
+    numberOfConcurrentThreads--;
+
     srand(0);
-    long arr_size = 100;
+    long arr_size = 1000000;
     int* arr = new int[arr_size];
-    for (long i = 0; i < arr_size; i++) {
+    for (long i = 0; i < arr_size; i++) 
+    {
         arr[i] = rand() % 500000;
     }
 
+    //cout << "Given array is \n";
+    //printArray(arr, arr_size);
+
     time_t start, end;
 
-    //int arr[] = { 12, 11, 13, 5, 6, 7 };
-    //int arr_size = sizeof(arr) / sizeof(arr[0]);
-
-    cout << "Given array is \n";
-    printArray(arr, arr_size);
-
+    time(&start);
     mergeSort(arr, 0, arr_size - 1);
+    //mergeSortWithoutThreads(arr, 0, arr_size - 1);
+    time(&end);
 
-    //future<void> fut = async(launch::async, [&]() {
-    //    mergeSort(arr, 0, arr_size - 1);
-    //    });
-    
+    cout << test << "\n";
+    int seconds = difftime(end, start);
+    printf("Threads. The time: %d seconds\n", seconds);
 
-    cout << "\nSorted array is \n";
-    printArray(arr, arr_size);
+    time(&start);
+    //mergeSort(arr, 0, arr_size - 1);
+    mergeSortWithoutThreads(arr, 0, arr_size - 1);
+    time(&end);
 
-    //unsigned int n = std::thread::hardware_concurrency();
-    //std::cout << n << " concurrent threads are supported.\n";
+    seconds = difftime(end, start);
+    printf("No threads. The time: %d seconds\n", seconds);
+
+    //cout << "\nSorted array is \n";
+    //printArray(arr, arr_size);
 }
